b0d8898bcd571e6891af5c9223d0d1eb15684ec2
itdanja2025 <itdanja2025@gmail.com>
2026-07-08 10:33:35 +0900

    day03 1h

---
+++ day03/exam1.js
@@ -0,0 +1,41 @@
+console.log( 'JS 실행 ')
+// 한줄주석
+/* 여러줄주석 */
+
+// [1] 숫자타입  
+// 1. 정수 
+console.log( 10 )
+console.log( typeof 10 ) // typeof 자료 , 해당 자료의 타입명 확인 
+// 2. 실수 : 부동소수점( 컴퓨터가 실수 처리하는 방법 )
+console.log( 3.14 )
+console.log( typeof 3.14 )
+
+// [2] 문자열타입 
+// 1. ' , " , ` 감싼 자료 
+console.log( '안녕' )
+console.log( "안녕" )
+console.log( `안녕` )
+// 2. 문자열 연결 
+console.log( '안녕' , '하세요' )
+console.log( '안녕' + '하세요' )
+console.log( `안녕 ${ 3 }`) // 문자열 템플릿 ? 문자열내 코드 포함
+console.log( '안녕' + 3 )   // 문자열 + 숫자 => 문자열 
+let a = 10
+console.log( '안녕' + a )   // 문자열 + 변수(숫자) => 문자열 
+console.log( `안녕 ${ a }`) // 문자열 템플릿은 `백틱으로 감싼 자료 안에 ${} 이용하여 (표현식)코드 포함 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
