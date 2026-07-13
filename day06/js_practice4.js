5b69793828e72bf017e7288095ca9a619ebf1d6e
itdanja2025 <itdanja2025@gmail.com>
2026-07-13 10:59:50 +0900

    day06 2h

--- day06/js_practice4.js
+++ day06/js_practice4.js
@@ -39,3 +39,25 @@ for( let line = 1 ; line <= 5 ; line++ ){
     output = output + '\n' // vs output += '\n'
 } // for end 
 console.log( output )
+
+/*문제 3: 배열에서 특정 문자 찾기 (break 활용)
+다음 사용자 이름 배열에서 이름에 '솔' 이라는 글자가 들어간 첫 번째 사용자를 찾으면, 해당 사용자의 이름을 출력하고 반복문을 종료하시오.*/
+// * 반복문 과 배열 관계 : 배열의 인덱스는 0부터 자료들을 저장하는 번호 이므로 반복문에서 0부터 마지막인덱스까지
+let userNames = ['김하준', '이서아', '박솔민', '최도윤'];
+// 배열의 마지막 인덱스 : 배열명.length - 1 
+for( let index = 0 ; index <= userNames.length - 1 ; index++ ){
+    if( userNames[index].indexOf('솔') != -1 ){  // 문자열은 배열이다.
+        console.log( userNames[ index ] )
+        break // for탈출/종료 // .indexOf( 찾을값 ) : 찾을값이 존재하면 인덱스 반환하고 없으면 -1 반환
+    }
+} // for end 
+/*문제 4: 2차원 배열의 모든 요소 출력하기: 다음과 같은 2차원 배열(좌석표)이 있습니다. 중첩 for 반복문을 사용하여 모든 좌석의 값을 순서대로 출력하시오.*/
+// 2차원 배열( 배열의 중철 ) , for 중첩 == 행과열(엑셀)
+let seatLayout = [ ['A1', 'A2', 'A3'], ['B1', 'B2', 'B3'], ['C1', 'C2', 'C3']  ];
+// 1. 행 먼저 반복문 이용하여 조회한다.
+for( let row = 0 ; row <= seatLayout.length - 1 ; row++ ){ //  0행 부터 2행 까지 1씩 증가하면 행 출력 
+    // 2. 반복문 이용하여 열 조회한다.
+    for( let col = 0 ; col <= seatLayout[row].length - 1 ; col++ ){ // 0열 부터 2열 까지 1씩 증가하면서 열 출력 
+        console.log(  seatLayout[row][col] )
+    } // for2 end 
+} // for1 end 
